fe6ee8b40c828d971f33d66255b7a4fdb258da13
Bebomonky <bebomonky@gmail.com>
2024-11-06 19:06:36 -0500

    New drawType (draw in Absolute or Relative)

--- Extensions/imenu/gui/imenu_Base.lua
+++ Extensions/imenu/gui/imenu_Base.lua
@@ -19,6 +19,7 @@ function BASE:Create()
 	self._cursor = Vector();
 	self._Fcursor = Vector();
 	self._useRealMouse = true;
+	self._drawType = 0;
 end
 
 function BASE:SetOwner(entity)
@@ -65,6 +66,14 @@ end
 
 function BASE:DrawAfterParent(bool)
 	self._drawAfterParent = bool;
+end
+
+function BASE:SetDrawType(num)
+	self._drawType = num;
+end
+
+function BASE:GetDrawType()
+	return self._drawType;
 end
 
 function BASE:SetPos(x, y)
