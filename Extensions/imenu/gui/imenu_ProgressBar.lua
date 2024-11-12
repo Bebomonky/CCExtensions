731a75a4a81f20c40f85096e73de22a15da74a38
Bebomonky <bebomonky@gmail.com>
2024-11-12 15:12:44 -0500

    Moved locals to script (bad idea to have them local)

--- Extensions/imenu/gui/imenu_ProgressBar.lua
+++ Extensions/imenu/gui/imenu_ProgressBar.lua
@@ -1,7 +1,4 @@
 local PANEL = {};
-local min = 0;
-local max = 1;
-local completed = false;
 
 function PANEL:Create()
 	self:SetControlID("PROGRESSBAR");
@@ -12,7 +9,9 @@ function PANEL:Create()
 	self:OutlineColor(144);
 	self:SetFraction(0);
 	self:SetSize(100, 10);
-	completed = false;
+	self._completed = false;
+	self._min = 0;
+	self._max = 1;
 end
 
 function PANEL:SetText(str)
@@ -36,26 +35,26 @@ function PANEL:BGColor(index)
 end
 
 function PANEL:GetCompleted()
-	return completed;
+	return self._completed;
 end
 
 function PANEL:SetFraction(value)
-	min = value;
+	self._min = value;
 end
 
 function PANEL:GetFraction()
-	return min;
+	return self._min;
 end
 
 function PANEL:NextUpdate()
 	local world_pos = self:GetAbsolutePos();
 
-	local factor = math.min(min, max);
+	local factor = math.min(self._min, self._max);
 	local size = self:GetSize();
 	local barEnd = world_pos + Vector(size.X * factor, size.Y) - Vector(1, 1);
 
 	if not self:GetHide() then
-		if min ~= 0 then
+		if self._min ~= 0 then
 			if math.min(size.X, size.Y) >= 0 then
 				PrimitiveMan:DrawBoxFillPrimitive(self:GetScreen(), world_pos, barEnd, self.bgColor);
 			end
@@ -64,13 +63,13 @@ function PANEL:NextUpdate()
 		PrimitiveMan:DrawTextPrimitive(self:GetScreen(), world_pos, self.text, self.smallText, 0);
 	end
 
-	if completed == false then
-		if min >= max then
-			completed = true;
+	if self.completed == false then
+		if self.min >= self._max then
+			self._completed = true;
 			if (self.OnComplete) then
 				self:OnComplete();
-				min = 0;
-				completed = false;
+				self._min = 0;
+				self._completed = false;
 			end
 		end
 	end
